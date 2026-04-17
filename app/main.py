b3498409487dc8fe773bc8d56bb143d51d15b6f4
sirajoo12 <149673831+sirajoo12@users.noreply.github.com>
2026-04-17 16:57:05 +0100

    Set up FastAPI with authentication router

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from auth.router import router as auth_router
+from database.base import Base
+from database.session import engine
 
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(auth_router, prefix="/auth")
