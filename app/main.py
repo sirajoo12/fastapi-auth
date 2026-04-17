6b8e0c65591386d36d84633a9da59656cca8eef7
sirajoo12 <149673831+sirajoo12@users.noreply.github.com>
2026-04-17 18:56:59 +0100

    Remove table creation and clean up imports

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
-
 from auth.router import router as auth_router
-from database.base import Base
-from database.session import engine
 
 app = FastAPI()
 
-# create tables
-Base.metadata.create_all(bind=engine)
-
-# connect auth routes
 app.include_router(auth_router, prefix="/auth")
 
 
