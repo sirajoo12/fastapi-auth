59c51cd757c56f2988665e0ce4ddbb0121555606
sirajoo12 <149673831+sirajoo12@users.noreply.github.com>
2026-04-17 16:52:21 +0100

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel
 
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: str
+
+    class Config:
+        from_attributes = True
