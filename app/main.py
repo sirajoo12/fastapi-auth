dfcbd048f335fcd6c5127a5aced3604f0148a478
sirajoo12 <149673831+sirajoo12@users.noreply.github.com>
2026-04-17 17:30:16 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+
 from auth.router import router as auth_router
 from database.base import Base
 from database.session import engine
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
+# create database tables
+Base.metadata.create_all(bind=engine)
+
+# include auth routes
 app.include_router(auth_router, prefix="/auth")
+
+
+@app.get("/")
+def home():
+    return {"message": "Auth system running 🚀"}
