c1c2abca1364b1c14e2406f4f5baa5a6285b5db1
sirajoo12 <149673831+sirajoo12@users.noreply.github.com>
2026-04-17 16:39:09 +0100

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,9 @@
+from pydantic import BaseSettings
 
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    DATABASE_URL: str
+
+settings = Settings()
